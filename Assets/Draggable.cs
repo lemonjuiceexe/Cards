304ff55862287fe9e8d7f2068186e878f1ac317f
Bruhtek <bartosz.kas2005@gmail.com>
2023-08-03 15:06:33 +0200

    Add lerp transition to card dragging

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,25 @@ public class Draggable : MonoBehaviour
 {
     [SerializeField]
     private Camera mainCamera;
+    [SerializeField]
+    private float snapTime = 0.1f;
+    
+    
+    private float snapTimer = 0f;
 
-    private void OnMouseDrag()
-    {
-        gameObject.transform.position = (Vector2) mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    private void OnMouseDown() {
+        snapTimer = 0f;
+    }
+    
+    private void OnMouseDrag() {
+        Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        
+        snapTimer += Time.deltaTime;
+        if (snapTimer > snapTime) {
+            gameObject.transform.position = mousePosition;
+        }
+        else {
+            gameObject.transform.position = Vector3.Lerp(transform.position, mousePosition, snapTimer / snapTime);
+        }
     }
 }
